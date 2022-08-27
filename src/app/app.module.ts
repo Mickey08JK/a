73a2aa1d418cfa742f83fc0f5300c87e56909e6e
mickey08jk <mckee.marinecorps@gmail.com>
2022-08-27 12:54:22 -0500

    finish login page with email/pwd and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { SharedModule } from './shared/shared.module';
 import { HomePageComponent } from './home-page/home-page.component';
+import { UserModule } from './user/user.module';
 
-import { environment } from 'src/environments/environment';
 
 //Firebase imports
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 // import { 
 //   provideFirestore, 
 //   getFirestore, 
@@ -22,7 +23,6 @@ import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 //   } from '@angular/fire/firestore';
 // import { provideStorage, getStorage } from '@angular/fire/storage';
 // import { provideAuth,getAuth } from '@angular/fire/auth';
-// import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 // import { provideDatabase,getDatabase } from '@angular/fire/database';
 
 
@@ -36,9 +36,10 @@ import { FIREBASE_OPTIONS } from '@angular/fire/compat';
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
+    UserModule,
     AngularFireModule,
     AngularFireAuthModule,
-    // provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
     // provideFirestore(() => {
     //   const firestore = getFirestore();
     //   connectFirestoreEmulator(firestore, 'localhost', 8080);
